c3566f62ac64fde04f9f64c94e9cbebd6bd55328
Ida Turøy <ida.turoy@gmail.com>
2023-10-17 21:39:01 +0200

    do small adjustment to layout

--- src/App/App.js
+++ src/App/App.js
@@ -10,14 +10,10 @@ import TabNavigation from '../components/TabNavigation/TabNavigation';
 function App() {
   return (
     <div className="App">
-      <header className="App-Container Margin-Header">
-        <h1 className="Margin-Heading">Knitting calculator</h1>
-      </header>
+      <h1 className="Margin-Heading">Knitting calculator</h1>
       <div className='App-Container'>
         <TabNavigation />
       </div>
-
-
     </div>
   );
 }
